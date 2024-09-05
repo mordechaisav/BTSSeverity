b7b8e8fcd877039afd0103c54113ff94dbb8fc1d
mordechaisav <mordechy7@gmail.com>
2024-09-05 14:00:19 +0300

    add function to read from threats (json)

--- BTSSeverity/Threats.cs
+++ BTSSeverity/Threats.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using BTSSeverity.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTSSeverity
+{
+    public class Threats
+    {
+        public void StartThearts()
+        {
+            string filePath = "C:/Users/Dell-pc/source/repos/BTSSeverity/BTSSeverity/Threats.json";
+            string json = File.ReadAllText(filePath);
+            List<ThreatsModel> defenses = JsonConvert.DeserializeObject<List<ThreatsModel>>(json);
+
+
+
+        }
+
+    }
+}
