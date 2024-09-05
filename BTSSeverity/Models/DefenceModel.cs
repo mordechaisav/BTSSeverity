d3cfe6ab3f5efe5428708016a3d7d584fc80704f
mordechaisav <mordechy7@gmail.com>
2024-09-05 12:05:47 +0300

    add defence model for read for json

---
+++ BTSSeverity/Models/DefenceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTSSeverity.Models
+{
+    public class DefenceModel
+    {
+        public int MinSeverity { get; set; }
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public DefenceModel Left {  get; set; }
+        public DefenceModel Right { get; set; }
+    }
+}
