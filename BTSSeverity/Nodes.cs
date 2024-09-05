22094d072040afd018e31c1426dea01b2b0b638f
mordechaisav <mordechy7@gmail.com>
2024-09-05 11:35:45 +0300

    Add project files.

---
+++ BTSSeverity/Nodes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTSSeverity
+{
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int MinSeverity {  get; set; }
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public Node() { }
+
+        public Node(int minSeverity, int maxSeverity, List<string> defenses)
+        {
+            Left = null;
+            Right = null;
+            MinSeverity = minSeverity;
+            MaxSeverity = maxSeverity;
+            Defenses = defenses;
+        }
+    }
+}
